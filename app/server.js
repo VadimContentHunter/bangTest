c917e8342882d40210a7e7a1ad86d595485116d6
VadimContentHunter <project.k.vadim@gmail.com>
2024-10-07 14:44:14 +0300

    created custom global error for server

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-const setupWebSocketServer = require("./app/server/websocketServer");
+const setupWebSocketServer = require("./server/websocketServer");
 
 const app = express();
 const port = 8080;
-const pathClient = path.join(__dirname, "app", "client");
+const pathClient = path.join(__dirname, "client");
 const pathClientHtml = path.join(pathClient, "html");
 // const pathServer = path.join(__dirname, "server");
 
@@ -15,13 +15,25 @@ const pathClientHtml = path.join(pathClient, "html");
 app.use(express.static(pathClientHtml));
 
 // Обрабатываем запрос на корень ("/")
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
 // Обработка ошибок 404
 app.use((req, res) => {
   res.status(404).sendFile(path.join(pathClientHtml, "404.html")); // Или отправьте сообщение
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Логируем стек ошибки
+
+  const statusCode = err.statusCode || 500; // Если статус-код не определен, используем 500
+  const message = err.message || "Что-то пошло не так";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
 });
 
 // Запускаем HTTP сервер с Express
