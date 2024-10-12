44b7b275af87b402308e10ad79422bb815f0d21b
VadimContentHunter <project.k.vadim@gmail.com>
2024-10-12 18:09:44 +0300

    rename callbackHandlerResponse and callbackHandlerRequest for websocketClient

--- app/client/js/websocketClient.js
+++ app/client/js/websocketClient.js
@@ -7,23 +7,41 @@ class WebSocketClientError extends Error {
     }
 }
 
+/**
+ * Создаёт WebSocket клиент для подключения к серверу.
+ *
+ * @param {string} ip - IP-адрес сервера в формате '123.123.123.123:12345'.
+ * @param {function(string): void} callbackHandlerResponse - Функция-обработчик для обработки входящих сообщений от сервера.
+ *                                                         - Принимает Данные, полученные от сервера.
+ * @param {function(WebSocket): void} callbackHandlerRequest - Функция-обработчик, которая вызывается после успешного подключения к серверу.
+ *                                                           - Принимает Объект WebSocket, представляющий текущее соединение.
+ *
+ * @throws {WebSocketClientError} Если параметр `ip` не является строкой.
+ * @throws {WebSocketClientError} Если `callbackHandlerResponse` не является функцией.
+ * @throws {WebSocketClientError} Если `callbackHandlerRequest` не является функцией.
+ * @throws {WebSocketClientError} Если `ip` не соответствует шаблону '123.123.123.123:12345'.
+ *
+ * @example
+ * // Пример использования
+ * websocketClient("95.109.169.148:8080",
+ *   (data) => { // Обработчик ответа
+ *       console.log("Получено сообщение:", data);
+ *   },
+ *   (ws) => { // Обработчик запроса
+ *       console.log("Запрос к серверу выполнен", ws);
+ *   });
+ */
 function websocketClient(ip, callbackHandlerResponse, callbackHandlerRequest) {
     if (typeof ip !== "string") {
-        throw new WebSocketClientError(
-            "Invalid IP address for websocket client"
-        );
+        throw new WebSocketClientError("Invalid IP address for websocket client");
     }
 
     if (typeof callbackHandlerResponse !== "function") {
-        throw new WebSocketClientError(
-            "Invalid callbackHandlerResponse for websocket client"
-        );
+        throw new WebSocketClientError("Invalid callbackHandlerResponse for websocket client");
     }
 
     if (typeof callbackHandlerRequest !== "function") {
-        throw new WebSocketClientError(
-            "Invalid callbackHandlerRequest for websocket client"
-        );
+        throw new WebSocketClientError("Invalid callbackHandlerRequest for websocket client");
     }
 
     const ipPortPattern = /^(\d{1,3}\.){3}\d{1,3}:\d{1,5}$/;
@@ -39,12 +57,12 @@ function websocketClient(ip, callbackHandlerResponse, callbackHandlerRequest) {
     };
 
     ws.onmessage = (event) => {
-        callbackResponse(event.data);
+        callbackHandlerResponse(event.data);
     };
 
     ws.onclose = () => {
         console.log("Соединение закрыто");
     };
 
-    callbackRequest(wp);
+    callbackHandlerRequest(wp);
 }
