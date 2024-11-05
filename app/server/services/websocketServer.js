99dd5c5376f4807c64cf0e92da0137b442d7f0f5
VadimContentHunter <project.k.vadim@gmail.com>
2024-11-05 18:23:29 +0200

    added events

--- app/server/services/websocketServer.js
+++ app/server/services/websocketServer.js
@@ -11,7 +11,9 @@ const JsonRpcMethodHandler = require("./JsonRpcMethodHandler");
 const PlayroomHandlerError = require("../Errors/PlayroomHandlerError");
 const PlayroomHandler = require("../handlers/PlayroomHandler");
 const aResponseHandler = require("../interfaces/aResponseHandler");
-const { log } = require("console");
+const EventEmitter = require("events");
+class MyHookEmitter extends EventEmitter {}
+const myHooks = new MyHookEmitter();
 
 module.exports = function setupWebSocketServer(server, playroomHandler) {
     if (!(playroomHandler instanceof PlayroomHandler)) {
@@ -20,48 +22,55 @@ module.exports = function setupWebSocketServer(server, playroomHandler) {
 
     const wss = new WebSocket.Server({ server });
 
-    // Событие при установлении нового соединения
-    wss.on("connection", (ws, req) => {
-        const queryParams = url.parse(req.url, true).query;
-        const clientIp = req.socket.remoteAddress; // Получаем IP-адрес клиента
-        const ip = clientIp.startsWith("::ffff:") ? clientIp.slice(7) : clientIp;
-        console.log(`WebSocket: Новое соединение установлено с IP: ${ip}`);
-
-        // Отправляем сообщение всем клиентам, когда клиент подключается
+    // Подписка на хуки
+    myHooks.on("updateUserCount", () => {
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                ws.send(
+                client.send(
                     JsonRpcFormatter.serializeRequest("updateUserCount", {
                         quantity: playroomHandler.countPlayersOnline(),
                     })
                 );
             }
         });
+    });
+
+    // Событие при установлении нового соединения
+    wss.on("connection", (ws, req) => {
+        const queryParams = url.parse(req.url, true).query;
+        const clientIp = req.socket.remoteAddress; // Получаем IP-адрес клиента
+        const ip = clientIp.startsWith("::ffff:") ? clientIp.slice(7) : clientIp;
+        let sessionId = SessionHandler.getCreateSessionId(queryParams.cookies);
+        console.log(`WebSocket: Новое соединение установлено с IP: ${ip}`);
+
+        playroomHandler.connect(sessionId);
+        myHooks.emit("updateUserCount");
 
         // Слушаем сообщения от клиента
         ws.on("message", (message) => {
-            const sessionId = SessionHandler.getCreateSessionId(queryParams.cookies);
+            // sessionId = SessionHandler.getCreateSessionId(queryParams.cookies);
             try {
-                ws.send(
-                    JsonRpcFormatter.serializeRequest("updateSessionId", {
-                        sessionId: sessionId,
-                        maxAge: SessionHandler.sessionLifetime * 1000,
-                        path: "/",
-                    })
-                );
+                // ws.send(
+                //     JsonRpcFormatter.serializeRequest("updateSessionId", {
+                //         sessionId: sessionId,
+                //         maxAge: SessionHandler.sessionLifetime * 1000,
+                //         path: "/",
+                //     })
+                // );
 
                 const requestRpc = JsonRpcFormatter.deserializeRequest(message);
                 requestRpc.params.sessionId = sessionId;
                 requestRpc.params.playroomHandler = playroomHandler;
 
                 const jsonRpcMethodHandler = new JsonRpcMethodHandler(requestRpc);
-                if (jsonRpcMethodHandler.instance instanceof aResponseHandler) {
-                    ws.send(
-                        JsonRpcFormatter.serializeResponse(
-                            jsonRpcMethodHandler.instance.getResult(),
-                            requestRpc?.id
-                        )
-                    );
+                if (
+                    jsonRpcMethodHandler.instance instanceof aResponseHandler &&
+                    typeof requestRpc.id === "number"
+                ) {
+                    const result = jsonRpcMethodHandler.instance.getResult();
+                    if (result !== null) {
+                        ws.send(JsonRpcFormatter.serializeResponse(result, requestRpc?.id));
+                    }
                 }
 
                 // const currentUserUrl = SessionHandler.getSessionData(sessionId);
@@ -90,21 +99,11 @@ module.exports = function setupWebSocketServer(server, playroomHandler) {
         // Обрабатываем отключение клиента
         ws.on("close", () => {
             try {
-                const sessionId = SessionHandler.getCreateSessionId(queryParams.cookies);
+                // sessionId = SessionHandler.getCreateSessionId(queryParams.cookies);
                 playroomHandler.removePlayerOnlineBySession(sessionId);
                 // console.log(playroomHandler.countPlayersOnline());
-                
 
-                wss.clients.forEach((client) => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        ws.send(
-                            JsonRpcFormatter.serializeRequest(
-                                "updateUserCount",
-                                playroomHandler.countPlayersOnline()
-                            )
-                        );
-                    }
-                });
+                myHooks.emit("updateUserCount");
             } catch (error) {
                 if (error instanceof PlayroomHandlerError) {
                     console.log(error.message);
