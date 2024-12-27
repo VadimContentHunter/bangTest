67028a03466fdb38cd33620529cd4f62206a8c45
VadimContentHunter <project.k.vadim@gmail.com>
2024-12-27 20:30:47 +0200

    added properties collectionRolesCards and collectionCharactersCards

--- app/server/models/GameSessionHead.js
+++ app/server/models/GameSessionHead.js
@@ -3,11 +3,24 @@ const PlayerCollection = require("../handlers/PlayerCollection");
 const Player = require("./Player");
 const DistanceError = require("../Errors/DistanceError");
 const DistanceHandler = require("../handlers/DistanceHandler");
+const CardsCollection = require("../handlers/CardsCollection");
 
 class GameSessionHead {
     #statusGame = false;
+    #collectionRolesCards = null;
+    #collectionCharactersCards = null;
 
-    constructor() {}
+    constructor() {
+        this.#collectionRolesCards =
+            this.#collectionRolesCards === null
+                ? new CardsCollection()
+                : this.#collectionRolesCards;
+
+        this.#collectionCharactersCards =
+            this.#collectionCharactersCards === null
+                ? new CardsCollection()
+                : this.#collectionCharactersCards;
+    }
 
     /**
      * Получить текущий статус игры.
@@ -29,15 +42,61 @@ class GameSessionHead {
         this.#statusGame = status;
     }
 
+    /**
+     * Получить коллекцию карт ролей.
+     * @returns {CardsCollection} Коллекция карт ролей.
+     */
+    get collectionRolesCards() {
+        return this.#collectionRolesCards;
+    }
+
+    /**
+     * Установить коллекцию карт ролей.
+     * @param {CardsCollection} collection - Новая коллекция карт ролей.
+     * @throws {TypeError} Если передано не значение типа CardsCollection.
+     */
+    set collectionRolesCards(collection) {
+        if (!(collection instanceof CardsCollection)) {
+            throw new TypeError("collectionRolesCards должен быть экземпляром CardsCollection.");
+        }
+        this.#collectionRolesCards = collection;
+    }
+
+    /**
+     * Получить коллекцию карт персонажей.
+     * @returns {CardsCollection} Коллекция карт персонажей.
+     */
+    get collectionCharactersCards() {
+        return this.#collectionCharactersCards;
+    }
+
+    /**
+     * Установить коллекцию карт персонажей.
+     * @param {CardsCollection} collection - Новая коллекция карт персонажей.
+     * @throws {TypeError} Если передано не значение типа CardsCollection.
+     */
+    set collectionCharactersCards(collection) {
+        if (!(collection instanceof CardsCollection)) {
+            throw new TypeError(
+                "collectionCharactersCards должен быть экземпляром CardsCollection."
+            );
+        }
+        this.#collectionCharactersCards = collection;
+    }
+
     toJSON() {
         return {
             statusGame: this.statusGame,
+            collectionRolesCards: this.collectionRolesCards, // можно сериализовать, если нужно
+            collectionCharactersCards: this.collectionCharactersCards, // можно сериализовать, если нужно
         };
     }
 
     static initFromJSON(json) {
         const gameSessionHeadHandler = new GameSessionHead();
         gameSessionHeadHandler.statusGame = json.statusGame;
+        gameSessionHeadHandler.collectionRolesCards = json.collectionRolesCards;
+        gameSessionHeadHandler.collectionCharactersCards = json.collectionCharactersCards;
         return gameSessionHeadHandler;
     }
 }
